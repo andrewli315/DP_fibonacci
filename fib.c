159295996f487e17ceb644bbab854a8b765bfb1a
Andrew Li <nyps70414@gmail.com>
2017-10-13 13:09:35 +0800

    resovle integer overflow

--- fib.c
+++ fib.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-int fibonacci(int n);
-static int fib[100];
+#include <windows.h>
+
+__int64 fibonacci(int n);
+static __int64 fib[100];
 
 int main(void)
 {
-	int x,input,i;
+	__int64 x;
+	int input,i;
 	for (i = 0;i<100;i++)
 		fib[i] = -1;
 	scanf("%d",&input);
+	LARGE_INTEGER t1, t2, ts;
+    QueryPerformanceFrequency(&ts);
+    QueryPerformanceCounter(&t1);
 	x = fibonacci(input);
-	printf("fibonacci level %d is %d\n",input, x);
+	QueryPerformanceCounter(&t2);
+	printf("fibonacci level %d is %lld\n",input, x);
+	printf("Lasting Time: %lf\n",(t2.QuadPart-t1.QuadPart)/(double)(ts.QuadPart));
 }
-int fibonacci(int n)
+__int64 fibonacci(int n)
 {
 	if(n == 1 || n ==0 )
 	{
@@ -26,6 +33,10 @@ int fibonacci(int n)
 		if(fib[n] == -1)
 		{
 			fib[n] = fibonacci(n-1)+fibonacci(n-2);
+		}
+		else if (n >= 100)
+		{
+			return fibonacci(n-1)+fibonacci(n-2);
 		}
 		else
 		{
